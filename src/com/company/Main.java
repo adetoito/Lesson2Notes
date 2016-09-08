82671db811bcf040f5d0a9169940cc013398b8a8
Argen Gian I Detoito <ad100@pvhs-lab112-d07.pvsd.org>
2016-09-08 14:29:17 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Declaring variables
+        // Types of variables:
+        // String, int, or double
+
+        String s;
+        s = "name"; // Initialization
+
+        String s2 = "nickname";
+
+        System.out.print(s);
+
+        // Naming rules of variables
+        // 1) Avoid all reserved words.
+            // if, else, void, public, static, private, etc.
+        // 2) Can't start with a number.
+            // 1banana isn't good.
+        // 3) Special characters (*&^*^&*)
+        // 4) Spaces are evil.
+            // First Name (NOT GOOD)
+            // firstName first_name (GOOD)
+        // 5) Reusing a variable name.
+
+        // String vs int
+        // int vs double
+
+        int a = 45;
+        double b = 3;
+        double sum = a + b;
+
+        a = 2_342_342;
+        int E, d = 0;
+        int sometime;
+        int thissss = 0;
+
+        String something = "";
+        // String have methods - much more advanced.
+    }
+}
